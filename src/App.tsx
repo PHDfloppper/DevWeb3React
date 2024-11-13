c883ae5a49657f4e2b44ece32e34cfa88af0dcf3
PHDfloppper <olivierbellerose1@gmail.com>
2024-11-13 16:05:36 -0500

    resolution de probleme avec netlify 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ListePerso from './components/ListePerso'
 import DetailPerso from './components/DetailsPerso'
